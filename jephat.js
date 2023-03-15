790e38034ac2d3e2768783276ce9bc2c8ae0376a
MJephat <mukundijephat22@gmail.com>
2023-03-15 10:45:17 +0300

    number one

---
+++ jephat.js
@@ -0,0 +1,13 @@
+//creating funtion
+function calculateSum(array) {
+    const Array = [56, 89,64, 74];
+    let sum = 0;
+    // iterate through the elements.
+    for (let i = 0; i < Array.length; i ++) {
+    //add array to sum
+      sum += Array[i];
+    }
+    
+return sum;
+  }
+  console.log(calculateSum([56, 89,64,74]));
